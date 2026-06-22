1d7498439f3510b1b0f5d1767965391d7a1fec22
Jeanrenel92 <jea.darius@duocuc.cl>
2026-06-21 21:11:36 -0400

    trabajando en test

---
+++ src/test/java/cl/duoc/api_compras/Model/OrdenModelTest.java
@@ -0,0 +1,21 @@
+package cl.duoc.api_compras.Model;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OrdenModelTest {
+    private static Validator validator;
+
+   // @BeforeAll
+   // static void initvalidation(){
+        //ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()
+    }
+
+   // @Test
+    //@DisplayName("Orden valido ")
+    //void ordenValido()
+//}
