9bb2adbc7fb60fa17c46dfe60f71c8bb1594600a
Jeanrenel92 <jea.darius@duocuc.cl>
2026-06-27 23:21:57 -0400

    Actualizacion simple

--- src/main/java/cl/duoc/api_compras/exception/GlobalExceptionHandler.java
+++ src/main/java/cl/duoc/api_compras/exception/GlobalExceptionHandler.java
@@ -2,8 +2,6 @@ package cl.duoc.api_compras.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
